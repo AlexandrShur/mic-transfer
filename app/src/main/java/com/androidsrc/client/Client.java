65fa0123b59bb4bc7bf6790bb59534d608346897
Konstantin Nikitin <kostya.nikitin@akvelon.com>
2017-11-24 21:58:55 +0300

    sources

---
+++ app/src/main/java/com/androidsrc/client/Client.java
@@ -0,0 +1,97 @@
+package com.androidsrc.client;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+public class Client extends AsyncTask<Void, Void, Void> {
+
+	String dstAddress;
+	int dstPort;
+	String response = "";
+	TextView textResponse;
+
+
+	public byte[] buffer;
+	AudioRecord audioRecord;
+
+	private int sampleRate = 16000;
+	private int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+	private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+
+	public boolean status  = true;
+
+	Client(String addr, int port,TextView textResponse) {
+		dstAddress = addr;
+		dstPort = port;
+		this.textResponse=textResponse;
+	}
+
+	@Override
+	protected Void doInBackground(Void... arg0) {
+
+		Socket socket = null;
+
+		try {
+			socket = new Socket(dstAddress, dstPort);
+			someK(socket);
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response = "UnknownHostException: " + e.toString();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response = "IOException: " + e.toString();
+		} finally {
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+
+	void someK(Socket socket) {
+		int minBufSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+
+		byte[] buffer = new byte[minBufSize];
+
+		audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, channelConfig, audioFormat, minBufSize);
+		audioRecord.startRecording();
+		while (status) {
+			minBufSize = audioRecord.read(buffer, 0, buffer.length);
+			try {
+				OutputStream ostream = socket.getOutputStream();
+				ostream.write(buffer, 0, buffer.length);
+				ostream.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			textResponse.setText(response);
+		}
+	}
+	@Override
+	protected void onPostExecute(Void result) {
+		textResponse.setText(response);
+		super.onPostExecute(result);
+	}
+}
---
+++ app/src/main/java/com/androidsrc/client/MainActivity.java
@@ -0,0 +1,60 @@
+package com.androidsrc.client;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class MainActivity extends Activity {
+
+	Client myClient;
+
+	TextView response;
+	EditText editTextAddress, editTextPort;
+	Button buttonConnect, buttonClear;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		editTextAddress = (EditText) findViewById(R.id.addressEditText);
+		editTextPort = (EditText) findViewById(R.id.portEditText);
+		buttonConnect = (Button) findViewById(R.id.connectButton);
+		buttonClear = (Button) findViewById(R.id.clearButton);
+		response = (TextView) findViewById(R.id.responseTextView);
+
+		buttonConnect.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				 myClient = new Client(editTextAddress.getText()
+						.toString(), Integer.parseInt(editTextPort
+						.getText().toString()), response);
+				myClient.execute();
+			}
+		});
+
+		buttonClear.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				myClient.status = false;
+				//response.setText("");
+			}
+		});
+	}
+
+}
